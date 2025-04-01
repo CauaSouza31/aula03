a47395665e0d0dbd7110f845f45d110b6ea3a1df
Cauã Souza <cauasouza3112@gmail.com>
2025-04-01 14:47:54 -0300

    Envio do segundo exercicio

--- aula03.py
+++ aula03.py
@@ -1,4 +1,9 @@
 nome = input("Informe seu nome: ")
 idade = int(input("infome sua idade: "))
 salario = float(input("Informe seu salario: "))
-print(f"Seu nome é {nome}, você tem {idade}anos e seu salario é {salario}")
+percentual = float(input("Digite sua percentual: "))
+valor_real = salario*(percentual/100)+ salario
+
+
+print(f"Seu nome é {nome}, você tem {idade} anos, seu salario é {salario}, com seu aumento ficaria {valor_real
+} ")
---
+++ exercicio02.py
@@ -0,0 +1,18 @@
+
+numero1 = int(input("Digite o numero: "))
+numero2 = int(input("Digite o segundo numero: "))
+
+soma = numero1+numero2
+subtração = numero1-numero2
+multiplicação = numero1*numero2
+divisão = numero1/numero2
+
+print(f"A soma do {numero1} + {numero2} = {soma}")
+print(f"A subtração do {numero1} - {numero2} = {subtração}")
+print(f"A multiplicação do {numero1} * {numero2} = {multiplicação}")
+print(f"A divisão do {numero1} / {numero2} = {divisão}")
+
+
+
+
+
