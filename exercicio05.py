196ed63594c73e3a27defe0f710c174a8219d1b8
Cauã Souza <cauasouza3112@gmail.com>
2025-04-01 15:56:35 -0300

    Envio do quinto exercicio

---
+++ exercicio05.py
@@ -0,0 +1,8 @@
+nota1 = float(input("Digite sua primeira nota: "))
+nota2 = float(input("Digite sua segunda nota: "))
+nota3 = float(input("Digite sua segunda nota: "))
+media=(nota1 + nota2 + nota3)/3
+    if media>=7:
+        print(f"Aprovado {media}")
+    else:
+        print(f"Reprovado {media}")
