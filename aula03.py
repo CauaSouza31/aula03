6535c02e816feb81c3674b1f30457153ab4a7992
Cauã Souza <cauasouza3112@gmail.com>
2025-04-01 14:27:40 -0300

    Adiconei mais informção no codigo

--- aula03.py
+++ aula03.py
@@ -1,4 +1,4 @@
 nome = input("Informe seu nome: ")
-idade = float(input("infome sua idade: "))
+idade = int(input("infome sua idade: "))
 salario = float(input("Informe seu salario: "))
 print(f"Seu nome é {nome}, você tem {idade}anos e seu salario é {salario}")
