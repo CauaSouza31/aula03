3afff16021b0d4889d611a1746185c516f359da5
Cauã Souza <cauasouza3112@gmail.com>
2025-04-01 15:46:31 -0300

    Envio do quarto exercicio

--- exercicio03.py
+++ exercicio03.py
@@ -1,5 +1,6 @@
 a = 5
 b = 10
+print(a,b)
 c = a
 a = b
 b = c
---
+++ exercicio04.py
@@ -0,0 +1,6 @@
+n1 = print("digite um numero: ")
+n2 = print("digite o segundo numero: ")
+if n1 > n2:
+    print(n1,n2)
+else:
+    print(n2,n1)
