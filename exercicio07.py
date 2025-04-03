85f341365d91c70ea8ee27680c584f85e07259f6
Cauã Souza <cauasouza3112@gmail.com>
2025-04-03 14:41:58 -0300

    oitavo exercicio

--- exercicio07.py
+++ exercicio07.py
@@ -2,7 +2,7 @@ tipo = input("Informe o tipo de combustivel: (e-etanal, g-gasolina)")
 litros = float(input("Quantos litros: "))
 vgaso = 5.80
 vEtal = 4.90
-
+valor = 0
 if tipo == 'G'or tipo =='g':
     valor = vgaso*litros
 else:
---
+++ exercicio08.py
@@ -0,0 +1,5 @@
+n = int(input("Informe um numero: "))
+if n%2 == 0:
+    print("Par")
+else:
+    print("Impar")
